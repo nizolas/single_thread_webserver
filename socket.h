fb5013945c561a8bc04d5f92317d17bbe13e1df6
nizolas <nizolas@gmail.com>
2014-09-17 00:33:38 -0400

    Header file for socket.cpp

---
+++ socket.h
@@ -0,0 +1,16 @@
+#include <netinet/in.h>
+
+class Socket
+{
+public:
+    // Private member variables.
+    int clientFD;
+    int serverFD;
+    int clientlen;
+    int portset;
+    struct sockaddr_in server, client;
+
+    Socket();
+    void init(char const *ipAddress, const int port);
+    int startSocket(char const *ipAddress, const int port, const bool debug = false);
+};
